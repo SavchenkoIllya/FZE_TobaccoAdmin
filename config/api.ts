657c742146083e21678dadde959f3e74ab976a92
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-18 02:30:25 +0200

    wip: filter shortness test 3

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,13 @@
 export default {
   rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
+    defaultLimit: 100,
+    maxLimit: 250,
     withCount: true,
+    query: {
+      parser: {
+        depth: 10,
+        allowDots: true,
+      },
+    },
   },
 };
