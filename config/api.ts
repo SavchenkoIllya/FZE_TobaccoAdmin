4dc96b156f67afe9b242317560e14b1f8192ed07
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-18 03:07:28 +0200

    wip: filter test 6

--- config/api.ts
+++ config/api.ts
@@ -1,12 +1,7 @@
 export default {
   rest: {
-    defaultLimit: 100,
-    maxLimit: 250,
+    defaultLimit: 25,
+    maxLimit: 100,
     withCount: true,
-    parser: {
-      parameterLimit: 10000,
-      arrayLimit: 100,
-      depth: 20,
-    },
   },
 };
